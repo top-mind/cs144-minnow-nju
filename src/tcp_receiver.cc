a039cdbab2c0dc01ed19cdef5889e62f348ab819
isapo <Tiancheng Geng>
2024-10-24 23:20:42 +0800

    check2

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -4,12 +4,40 @@ using namespace std;
 
 void TCPReceiver::receive( TCPSenderMessage message )
 {
-  // Your code here.
-  (void)message;
+  if (message.RST) {
+    // XXX who's error?
+    reassembler_.reader().set_error();
+    return;
+  }
+
+  uint64_t index = 0;
+
+  if (message.SYN) {
+    isn_ = message.seqno + 1; // index 0 starts at seqno ISN + 1
+  } else {
+    if (!isn_.has_value())
+      return;
+    uint64_t checkpoint = reassembler_.writer().bytes_pushed();
+    index = message.seqno.unwrap(isn_.value(), checkpoint);
+  }
+
+  reassembler_.insert(index, message.payload, message.FIN);
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
-  // Your code here.
-  return {};
+  optional<Wrap32> op = {};
+  if (isn_.has_value()) {
+    op = Wrap32::wrap(reassembler_.writer().bytes_pushed(), isn_.value()) + reassembler_.writer().is_closed();
+  }
+  uint16_t window_size = UINT16_MAX;
+  uint64_t available_capacity = reassembler_.writer().available_capacity();
+  if (window_size > available_capacity) window_size = available_capacity;
+
+  TCPReceiverMessage ret;
+  ret.ackno = op;
+  ret.window_size = window_size;
+  ret.RST = reassembler_.writer().has_error();
+
+  return ret;
 }
--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,16 +4,18 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return zero_point + n;
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint32_t raw = raw_value_ - zero_point.raw_value_;
+  uint64_t sma = ((checkpoint - raw) >> 32 << 32) | raw;
+  uint64_t lar = sma + (1ull << 32);
+  if (lar < checkpoint) return sma;
+  if (sma > checkpoint) return lar;
+  if (lar - checkpoint >= checkpoint - sma)
+    return sma;
+  else
+    return lar;
 }
