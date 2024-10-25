e5e734c6237a04cd699227f5c4029af6c5dcca78
isapo <Tiancheng Geng>
2024-10-25 10:36:17 +0800

    check2 refactor + format

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -2,28 +2,25 @@
 // #include <iostream>
 
 using namespace std;
-void Reassembler::insert( uint64_t __first_index, string data, bool __is_last_substring ) { if (output_.writer().is_closed())
+void Reassembler::insert( uint64_t __first_index, string data, bool __is_last_substring )
+{
+  if ( output_.writer().is_closed() )
     return;
-  const uint64_t
-    _1 = output_.writer().bytes_pushed(),
-    _2 = output_.writer().available_capacity(),
-    _3 = data.size();
+  const uint64_t _1 = output_.writer().bytes_pushed(), _2 = output_.writer().available_capacity(), _3 = data.size();
 
-  if (__is_last_substring) {
+  if ( __is_last_substring ) {
     end_pos_ = __first_index + _3;
     end_pos_known_ = true;
 
-    if (output_.writer().bytes_pushed() == end_pos_)
+    if ( output_.writer().bytes_pushed() == end_pos_ )
       output_.writer().close();
   }
 
-  uint64_t
-    l = max(__first_index, _1),
-    r = min(__first_index + _3, _1 + _2 ),
-    _l = l - __first_index,
-    len = r - l;
+  uint64_t l = max( __first_index, _1 ), r = min( __first_index + _3, _1 + _2 ), _l = l - __first_index,
+           len = r - l;
 
-  if (l >= r) return;
+  if ( l >= r )
+    return;
 
   bool is_trunc = _l != 0 || len != _3;
 
@@ -35,25 +32,25 @@ void Reassembler::insert( uint64_t __first_index, string data, bool __is_last_su
   // - +
   // + -
   // - -
-  
-  auto _4 = mdata.lower_bound(l), _5 = _4;
+
+  auto _4 = mdata.lower_bound( l ), _5 = _4;
   // cover case
-  if (_4 != mdata.end() && _4->first == l && l + _4->second.size() >= r)
+  if ( _4 != mdata.end() && _4->first == l && l + _4->second.size() >= r )
     return;
 
   // erase all datas covered by @__data
-  for (; _5 != mdata.end() && _5->first + _5->second.size() <= r; _5++)
+  for ( ; _5 != mdata.end() && _5->first + _5->second.size() <= r; _5++ )
     size_ -= _5->second.size();
-  mdata.erase(_4, _5);
+  mdata.erase( _4, _5 );
 
   // update truncate
   _4 = _5;
-  if (_4 != mdata.begin()) {
+  if ( _4 != mdata.begin() ) {
     --_4;
     uint64_t _4r = _4->first + _4->second.size();
-    if (_4r >= r)
+    if ( _4r >= r )
       return;
-    if (l < _4r) {
+    if ( l < _4r ) {
       l = _4r;
       _l = l - __first_index;
       len = r - l;
@@ -61,31 +58,31 @@ void Reassembler::insert( uint64_t __first_index, string data, bool __is_last_su
     }
   }
 
-  if (_5 != mdata.end()) {
-    if (r > _5->first) {
+  if ( _5 != mdata.end() ) {
+    if ( r > _5->first ) {
       r = _5->first;
       len = r - l;
       is_trunc = true;
     }
   }
 
-  mdata.emplace_hint(_5, l, is_trunc ? data.substr(_l, len) : move(data));
+  mdata.emplace_hint( _5, l, is_trunc ? data.substr( _l, len ) : move( data ) );
   size_ += len;
 
   // write
-  while (!mdata.empty()) {
+  while ( !mdata.empty() ) {
     auto i = mdata.begin();
     auto [idx, str] = *i;
-    if (idx == output_.writer().bytes_pushed()) {
+    if ( idx == output_.writer().bytes_pushed() ) {
       size_ -= str.size();
-      output_.writer().push(move(str));
-      mdata.erase(i);
+      output_.writer().push( move( str ) );
+      mdata.erase( i );
     } else {
       break;
     }
   }
 
-  if (end_pos_known_ && output_.writer().bytes_pushed() == end_pos_)
+  if ( end_pos_known_ && output_.writer().bytes_pushed() == end_pos_ )
     output_.writer().close();
 }
 
--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -10,11 +10,13 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   uint32_t raw = raw_value_ - zero_point.raw_value_;
-  uint64_t sma = ((checkpoint - raw) >> 32 << 32) | raw;
-  uint64_t lar = sma + (1ull << 32);
-  if (lar < checkpoint) return sma;
-  if (sma > checkpoint) return lar;
-  if (lar - checkpoint >= checkpoint - sma)
+  uint64_t sma = ( ( checkpoint - raw ) >> 32 << 32 ) | raw;
+  uint64_t lar = sma + ( 1ull << 32 );
+  if ( lar < checkpoint )
+    return sma;
+  if ( sma > checkpoint )
+    return lar;
+  if ( lar - checkpoint >= checkpoint - sma )
     return sma;
   else
     return lar;
